9086cd09eb1486699bc8bdad445911069d23b003
Ben Leong <cleong@ets.org>
2021-03-11 17:48:20 -0500

    cleaned up

--- openface-cambridge-2.2.0/start_feature_extraction.sh
+++ openface-cambridge-2.2.0/start_feature_extraction.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
  
 USER=cleong
-MACHINE=saturn
-SERVER_DATA_DIR=/home/nlp-speech/static/Corpora/multimodal_assessments/data/test
-DOCKER_DATA_DIR=/opt/OpenFace/build/bin/data
+MACHINE=MACHINE_NAME
+SERVER_DATA_DIR=YOUR_PATH_TO_SERVER_DATA
+DOCKER_DATA_DIR=YOUR_PATH_TO_DOCKER_DATA
 VIDEO_DATA_DIR=video
  
 mkdir -p $DOCKER_DATA_DIR
  
-sshfs -o idmap=user $USER@$MACHINE.research.ets.org:$SERVER_DATA_DIR $DOCKER_DATA_DIR
+sshfs -o idmap=user $USER@$MACHINE.XXX.XX.ORG:$SERVER_DATA_DIR $DOCKER_DATA_DIR
  
 ./FeatureExtraction -q -f $DOCKER_DATA_DIR/$VIDEO_DATA_DIR/test.mp4 -out_dir $DOCKER_DATA_DIR/$VIDEO_DATA_DIR -of test.features
 
